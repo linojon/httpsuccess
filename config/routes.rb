959a1ee23606697f7dd5af045813ba08f26bbbaa
Jonathan Linowes <linojon@gmail.com>
2012-07-26 20:57:47 -0400

    Initial pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Httpsuccess::Application.routes.draw do
+  
+  match 'about' => 'pages#about'
+  root :to => 'pages#home'
+
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
