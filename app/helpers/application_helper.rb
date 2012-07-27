452572e6ab817d2286ee9b486abb44743ec1f802
Jonathan Linowes <linojon@gmail.com>
2012-07-26 20:57:47 -0400

    Added layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+  def title(page_title, show_title = true)
+    content_for(:title) { h(page_title.to_s) }
+    @show_title = show_title
+  end
+
+  def show_title?
+    @show_title
+  end
+
+  def body_id(id)
+    content_for(:body_id) { id }
+  end
+
 end
